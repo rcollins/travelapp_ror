66d69852a20eee083743cdc45044df3511a3216c
Robert Collins <robert.collinsjr@gmail.com>
2017-02-07 02:06:43 +0000

    Added info view, route, controller and fixed mobile view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
   
   def vendors
   end
+  
+  def info
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   root to:'pages#home'
   get 'vendors', to: 'pages#vendors'
+  get 'info', to: 'pages#info'
   
 end
