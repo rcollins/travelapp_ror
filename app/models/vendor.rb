75fae8d6422543aba317dbb51ce6068932448b08
Robert Collins <robert.collinsjr@gmail.com>
2017-02-08 02:35:50 +0000

    Added vendor form validations to model

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,3 +1,6 @@
 class Vendor < ActiveRecord::Base
-  
+  validates :vendor_name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :vendor_url, presence: true
 end
