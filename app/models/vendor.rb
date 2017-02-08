6ac0d0f9063b673951c09f9413c72ebc6477d99f
Robert Collins <robert.collinsjr@gmail.com>
2017-02-08 07:40:21 +0000

    Modified the vendor model file

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,6 +1,4 @@
 class Vendor < ActiveRecord::Base
   validates :vendor_name, presence: true
   validates :username, presence: true
-  validates :password, presence: true
-  validates :vendor_url, presence: true
 end
